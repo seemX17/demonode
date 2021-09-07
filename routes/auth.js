ae0308094bd4ee967c3384f4287ccf9219db50e5
seemranxec <seemran.xec@gigacars.com>
2021-09-07 19:36:10 +0530

    sequelize

--- routes/auth.js
+++ routes/auth.js
@@ -1,21 +1,41 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const config = require("../config")
-
+const config = require("../config");
+const models = require("../models");
 
 const authRoutes = (app, passport) => {
     app.post('/auth/login',
         async (request, response) => {
 
-
-            var jwtPayload = {
-                id: 2,
-                email: "simxec@gmail.com"
+            let username = request.body.username;
+            let password = request.body.password;
+            console.log(request.body)
+            let user = await models.users.findOne({
+                where: {
+                    email: username,
+                }
+            });
+            if (!user) {
+                response.status(404).send("User doesn't exist!");
             }
 
-            let token = jwt.sign(jwtPayload, config.secret)
+            if (!(await bcrypt.compare(password, user.password))) {
+                response.status(400).send("Incorrect password!");
+            }
 
-            response.send(token)
+            var jwtPayload = {
+                id: user.id,
+                email: user.email
+            }
+            let token = jwt.sign(jwtPayload, config.secret)
+            response.json({
+                token,
+                user: {
+                    name: user.name,
+                    email: user.email,
+                    createdAt: user.createdAt
+                }
+            })
 
 
             // console.log(request.body)
@@ -29,6 +49,34 @@ const authRoutes = (app, passport) => {
     }), async (request, response) => {
         response.send("success")
     })
+
+    app.post("/auth/signup", async (request, response) => {
+        let name = request.body['name'];
+        let pass = request.body['password'];
+        let email = request.body['email'];
+        let user = await models.users.create({
+            name,
+            password: await bcrypt.hash(pass, 10),
+            email
+        });
+
+        if (!user) {
+            response.status(400);
+        }
+        response.json({
+            name: user.name,
+            //password: user.password,
+            email: user.email,
+            createdAt: user.createdAt
+        })
+    })
 }
 
 module.exports.authRoutes = authRoutes;
+
+//POSt GRES
+//create database dbname
+// \c databasename
+// squalize cli, squelize init
+// sequelize migration:generate --name create-users
+// nodemon
