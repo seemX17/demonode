3c2e3042b3d4245e7334cc380810a01b567ee964
seemranxec <seemran.xec@gigacars.com>
2021-09-07 19:45:18 +0530

    jwt with db

--- passport.js
+++ passport.js
@@ -1,5 +1,6 @@
 const passport = require("passport")
 const config = require("./config")
+const models = require("./models");
 
 var JwtStrategy = require('passport-jwt').Strategy,
     ExtractJwt = require('passport-jwt').ExtractJwt;
@@ -12,24 +13,21 @@ opts.secretOrKey = config.secret;
 var strategy = new JwtStrategy(opts, function (jwt_payload, next) {
     console.log("payload received", jwt_payload);
     // usually this would be a database call:
+    models.users.findOne({
+        where: {
+            id: jwt_payload.id,
+            email: jwt_payload.email
+        }
+    }).then(user => {
+        if (!user) {
+            next(null, false)
+        }
+        next(null, user)
+    });
 
-    next(null, {
-        id: 3
-    })
 
-    // models.user
-    //   .findOne({
-    //     where: {
-    //       id: jwt_payload.id
-    //     }
-    //   })
-    //   .then(user => {
-    //     if (user) {
-    //       next(null, user);
-    //     } else {
-    //       next(null, false);
-    //     }
-    //   });
+
+
 });
 
 passport.use(strategy);
